2726976e1f61983290cd42713a454443d0e44237
dangngocan01031999 <72962277+dangngocan01031999@users.noreply.github.com>
2022-11-26 17:46:49 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -88,7 +88,7 @@ IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
-FIRST_PORT=22251
+FIRST_PORT=22301
 LAST_PORT=22500
 
 gen_data >$WORKDIR/data.txt
